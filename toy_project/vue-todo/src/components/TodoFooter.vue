10618c4a36ecf1f4b3d07c8673fdac5117b252e0
leejaypower <devejayloper@gmail.com>
2022-04-19 23:16:48 +0900

    ✨ 할 일 모두 삭제 기능

---
+++ toy_project/vue-todo/src/components/TodoFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="clearAllContainer">
+    <button type="button" class="clearAllBtn shadow" v-on:click="clearAllTodo">clear All</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearAllTodo() {
+      localStorage.clear();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.clearAllContainer{
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+
+button{
+  border: 0;
+  width: inherit;
+  height: inherit;
+  border-radius: 30px;
+}
+.clearAllBtn{
+  display: block;
+  background-color: white;
+  color: tomato;
+}
+</style>
