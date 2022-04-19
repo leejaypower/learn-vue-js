012c38352f6de99ac53ccda975c11ea2b65c9973
leejaypower <devejayloper@gmail.com>
2022-04-19 14:47:47 +0900

    🎉Todo list App 프로젝트

---
+++ toy_project/vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+  <todo-header></todo-header>
+  <todo-input></todo-input>
+  <todo-list></todo-list>
+  <todo-footer></todo-footer>
+</div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
