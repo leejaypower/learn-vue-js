31e736d04f4eb591bb4f800b91b1686e2f41949d
leejaypower <devejayloper@gmail.com>
2022-05-05 13:20:29 +0900

    💄 vue 리스트 트랜지션 추가해보기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <ul>
+    <transition-group
+      name="list"
+      tag="ul"
+    >
       <li
-        v-for="(todoItem, index) in propsdata"
+        v-for="(todoItem, index) in todoItems"
         :key="todoItem.item"
         class="shadow"
       >
@@ -19,13 +22,18 @@
           <i class="fas fa-trash-alt" />
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['propsdata'],
+  props: {
+    todoItems: {
+      type: Array,
+      default: () => [],
+    },
+  },
   methods: {
     removeTodo(todoItem, index) {
       this.$emit('removeItem', todoItem, index);
@@ -77,4 +85,11 @@ li {
   color:#b3adad;
 }
 
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
